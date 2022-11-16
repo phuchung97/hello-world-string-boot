00d52131e9f42a4566570dc17aaf56785a5fb753
Phúc Hưng Triệu <you@example.com>
2022-11-16 12:35:04 +0900

    get index function

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+
+public class Controller {
+	
+	@GetMapping("/")
+	public String get_index() {
+		return "index";
+	}
+
+}
